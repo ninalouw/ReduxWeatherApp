549d3a7a9cabaef9bb217466566522f64697e699
Nina Louw <ninalouw@mac.com>
2017-02-20 18:45:38 +0200

    add middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 const API_KEY = 'fbb921ec0d009ea1349b0531b5cb3538';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city){
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
+
+  // console.log('Request', request);
+
   return {
     type:FETCH_WEATHER,
     payload: request
